30b57f82d3bf22070b26b7ab07b4d3050d13a184
MohamedRagabJr <muhamidragab201020@gmail.com>
2025-05-11 17:14:54 +0300

    first commit

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+export default function ProdctList() {
+  return (
+    <>
+      <h1>Lorem, ipsum.</h1>
+      <h2>Lorem, ipsum.</h2>
+      <h3>Lorem, ipsum.</h3>
+      <h4>Lorem, ipsum.</h4>
+      <h5>Lorem, ipsum.</h5>
+    </>
+  );
+}
